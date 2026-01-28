a2ece1ea14b6b439d1fdbf6b3c3bbaf4211ac8ce
oioioioioioi666 <qing.xu@bsu.edu>
2026-01-27 22:30:12 -0500

    commit

--- sample-express/index.js
+++ sample-express/index.js
@@ -1,11 +1,36 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { routeHello, routeAPINames, routeWeather } from "./routes.js";
 import express from "express";
-
-const app = express();
+const server = express();
 const port = 3000;
-app.get("/hello", (req, res) => {
-  res.send("Hello! This is my custom Express greeting.");
+server.get("/hello", function (_req, res) {
+    const response = routeHello();
+    res.send(response);
 });
-
-app.listen(port, () => {
-  console.log("Listening on " + port);
+server.get("/api/names", function (_req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let response;
+        try {
+            response = yield routeAPINames();
+            res.send(response);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+});
+server.get("/api/weather/:zipcode", function (req, res) {
+    const response = routeWeather({ zipcode: String(req.params.zipcode) });
+    res.send(response);
+});
+server.listen(port, function () {
+    console.log("Listening on " + port);
 });
--- sample-express/routes.js
+++ sample-express/routes.js
@@ -1,19 +1,35 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import fetch from "node-fetch";
-export const hello = () => (req, res) => {
-res.send("Hello! This is my custom greeting from Express!");
+const routeHello = () => "Hello World!";
+const routeAPINames = () => __awaiter(void 0, void 0, void 0, function* () {
+    const url = "https://www.usemodernfullstack.dev/api/v1/users";
+    let data;
+    try {
+        const response = yield fetch(url);
+        data = (yield response.json());
+    }
+    catch (err) {
+        return "Error";
+    }
+    const names = data
+        .map((item) => `id: ${item.id}, name: ${item.name}`)
+        .join("<br>");
+    return names;
+});
+const routeWeather = (query) => queryWeatherData(query);
+const queryWeatherData = (query) => {
+    return {
+        zipcode: query.zipcode,
+        weather: "sunny",
+        temp: 35
+    };
 };
-export const apiNames = () => async (req, res) => {
-  const url = "https://www.usemodernfullstack.dev/api/v1/users";
-  let data;
-
-  try {
-    const response = await fetch(url);
-    data = await response.json(); 
-  } catch (err) {
-    return res.status(500).send("API fetch failed.");
-  }
-
-  const rows = data.map((u) => `${u.name} - ${u.id}`).join("<br>");
-
-  res.send(rows);
-};
+export { routeHello, routeAPINames, routeWeather };
